e48c00aa49cb3038d751aa272bb94ea957d96706
ddort1s <ivanViolinist@yandex.ru>
2025-11-10 10:56:55 +0300

    New Commit

---
+++ src/Main2.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("stop")) {
+                break;
+            }
+
+            int countShelfBooks;
+            try {
+                countShelfBooks = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            if (!scanner.hasNextLine()) {
+                break;
+            }
+            String targetBook = scanner.nextLine();
+
+            if (targetBook.equalsIgnoreCase("stop")) {
+                System.out.println("You checked 0 books and did not find " + targetBook);
+                break;
+            }
+
+            boolean found = false;
+            int checkedBooks = 0;
+
+            for (int i = 0; i < countShelfBooks; i++) {
+                if (!scanner.hasNextLine()) {
+
+                    System.out.println("You checked " + checkedBooks + " books and did not find " + targetBook);
+                    return;
+                }
+
+                String currentInput = scanner.nextLine();
+
+                if (currentInput.equalsIgnoreCase("stop")) {
+                    System.out.println("You checked " + checkedBooks + " books and did not find " + targetBook);
+                    return;
+                }
+
+                try {
+                    Integer.parseInt(currentInput);
+
+                    System.out.println("You checked " + checkedBooks + " books and did not find " + targetBook);
+
+                    countShelfBooks = Integer.parseInt(currentInput);
+
+                    if (!scanner.hasNextLine()) {
+                        return;
+                    }
+                    targetBook = scanner.nextLine().trim();
+
+                    if (targetBook.equalsIgnoreCase("stop")) {
+                        System.out.println("You checked 0 books and did not find " + targetBook);
+                        return;
+                    }
+
+                    found = false;
+                    checkedBooks = 0;
+                    i = -1;
+                } catch (NumberFormatException e) {
+                    checkedBooks++;
+
+                    if (currentInput.equals(targetBook)) {
+                        System.out.println("Checked " + checkedBooks + " books and found " + targetBook);
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found && checkedBooks == countShelfBooks) {
+                System.out.println("You did not find " + targetBook);
+            }
+        }
+    }
+}
