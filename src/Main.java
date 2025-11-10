fd0cc1bef1a7d6f2dc7e4558b9515032e2d3de91
ddort1s <141556284+ddort1s@users.noreply.github.com>
2025-11-10 15:51:04 +0300

    Update Main.java
    Code Rewie

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = 100;
-
-        int minPrime = Integer.MAX_VALUE;
+        int minPrime = -1;  
         long productNonPrime = 1;
         boolean hasNonPrime = false;
-
+        
         while (true) {
             String input = scanner.next();
-
             if (input.equals("stop")) {
                 break;
             }
-
             int num;
             num = Integer.parseInt(input);
-
+            
             if (num <= 1 || num == N) {
                 System.out.println("invalid");
                 continue;
             }
-
+            
             if (isPrime(num)) {
-                if (num < minPrime) {
+                if (minPrime == -1 || num < minPrime) {  
                     minPrime = num;
                 }
             } else {
@@ -33,18 +29,20 @@ public class Main {
                 hasNonPrime = true;
             }
         }
-        if (minPrime == Integer.MAX_VALUE) {
+        
+        if (minPrime == -1) {  
             System.out.println("prime_undefined");
         } else {
             System.out.println("prime_" + minPrime);
         }
-
+        
         if (!hasNonPrime) {
             System.out.println("non_prime_undefined");
         } else {
             System.out.println("non_prime_" + productNonPrime);
         }
     }
+    
     public static boolean isPrime(int num) {
         if (num < 2) return false;
         for (int i = 2; i <= Math.sqrt(num); i++) {
@@ -53,3 +51,4 @@ public class Main {
         return true;
     }
 }
+
